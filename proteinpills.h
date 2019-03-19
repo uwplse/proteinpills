9f77949560ba59f0271b6fd15d6c5e99690de63c
Doug Woos <doug.woos@gmail.com>
2019-03-19 15:55:46 -0700

    c++ compat

--- proteinpills.h
+++ proteinpills.h
@@ -1,11 +1,17 @@
 #ifndef __MAJORTOM
 #define __MAJORTOM
 
-void annotate_timeout(char *ty);
+#ifdef __cplusplus
+extern "C" {
+#endif
 void annotate_message(char *ty);
 void register_state_function(void (*)(void));
 
 void int_field(char *field, int value);
 void str_field(char *field, char *value);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
